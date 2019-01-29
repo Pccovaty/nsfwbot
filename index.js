f1e8ef49d4d63c49c11aee0e50722b4557336820
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2019-01-29 21:17:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const botconfig = require("./botconfig.json");
 const Discord = require('discord.js');
 const fs = require("fs");
+const moment = require('moment');
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 bot.mutes = [];
